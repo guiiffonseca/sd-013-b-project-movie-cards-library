0d11efe7a6ec86e972b34344fc2ee7b25bbf9bf5
Camila Dornas <camiladornas@gmail.com>
2021-07-22 17:30:31 -0300

    Requisito 3 e 16

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
-// implement MovieCard component here
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// // import movies from '../data';
+
+// const { movies } = this.props;
+
+// const MovieCard = () => (
+//   <li key={ value }>
+//     {' '}
+//     {value}
+//     {' '}
+//   </li>
+// );
+// export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <body>
+        <MovieCard />
+      </body>
+    );
+  }
+}
+
+export default MovieList;
