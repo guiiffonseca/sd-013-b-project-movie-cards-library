f87f61f732c06ee0ef0e31e0aaa2c1e7d3d1553a
Guilherme Giacomini Teixeira <ggiacomini2012@gmail.com>
2021-07-22 20:34:38 -0300

    conclui projeto :P

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,20 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    let { rating } = this.props;
+    if (!rating) {
+      rating = 0;
+    }
+    return (
+      <div className="rating">{ rating }</div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
+
+export default Rating;
