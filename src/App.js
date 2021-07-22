2da9b15ec1a765a0f23e9f0b8a77a32092f233c7
GuilhermeLaraRusso <guilhermelrusso@gmail.com>
2021-07-22 15:59:07 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <div className="movie-card-header">
         <Header />
       </div>
+      <MovieList />
     </div>
   );
 }
