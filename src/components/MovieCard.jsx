73132f5a24250fd34d6ce848fbe53e155c4efb0c
Lucas Caribé <lucasmoc47@gmail.com>
2021-07-22 17:24:13 -0300

    Estilizaçao padrao

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,18 @@ import Rating from './Rating';
 
 export default class MovieCard extends React.Component {
   render() {
-    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    const {
+      movie: { title, subtitle, storyline, rating, imagePath },
+    } = this.props;
 
     return (
       <div className="movie-card">
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
         <Rating rating={ rating } />
       </div>
     );
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -4,7 +4,11 @@ import PropTypes from 'prop-types';
 export default class Rating extends React.Component {
   render() {
     const { rating } = this.props;
-    return <div className="rating">{rating}</div>;
+    return (
+      <div className="movie-card-rating">
+        <div className="rating">{rating}</div>
+      </div>
+    );
   }
 }
 
