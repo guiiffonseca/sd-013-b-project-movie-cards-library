a45a670a420bc928bf55f641a6b5423ff354640f
guilherme <guilhermedc93@gmail.com>
2021-07-22 14:07:54 -0300

    Conclui requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,20 @@
 // implement Header component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  text: propTypes.string.isRequired,
+};
+
+
+export default Header;
