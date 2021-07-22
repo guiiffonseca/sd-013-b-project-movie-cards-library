5d411634b18f94587922977c6602ae46bc0639e5
JoãoBDf <joaobdf@hotmail.com>
2021-07-22 16:51:07 -0300

    Requisitos 4, 5, 6, 7, 8, 9, 10 finalizados

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
-        { movies }
+      <div className="movie-list">
+        { movies.map((value) => <MovieCard key={ value.title } movie={ value } />) }
       </div>
     );
   }
