2bde7d8bb51f6f961f868835cb6c122c95b22c0c
André <andrebpessoa09@gmail.com>
2021-07-22 17:24:38 -0300

    Requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
 // implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+
+    return (
+      <section className="movie-card">
+        <div className="movie-card-body">
+          <img src={ movie.imagePath } alt={ movie.title } className="movie-card-image" />
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
 // implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movie-list">
+        {movies.map((movie) => (
+          <MovieCard
+            key={ movie.title }
+            movie={ movie }
+          />
+        ))}
+      </section>
+    );
+  }
+}
+
+export default MovieList;
