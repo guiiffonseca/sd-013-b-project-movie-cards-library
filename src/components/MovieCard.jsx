7eb3da231bde48438440f166cf7a93569550241a
Lailson Gabriel <lailsongabrielturbo@gmail.com>
2021-07-22 16:28:06 -0300

    bomba

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return (
+      <div className="movie-card">
+        <img src={ imagePath } alt={ title } />
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
-        { movies }
+      <div className="movie-list">
+        { movies.map(({ title, subtitle, storyline, imagePath, rating }) => (
+          <MovieCard
+            key={ title }
+            subtitle={ subtitle }
+            storyline={ storyline }
+            imagePath={ imagePath }
+            rating={ rating }
+          />
+        )) }
       </div>
     );
   }
