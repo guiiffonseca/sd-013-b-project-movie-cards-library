46a60146d9601a71829ce5662d9176e32b6c31a9
Vini Queiroz <viniderp@gmail.com>
2021-07-22 16:39:46 -0300

    First Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,15 @@
-// implement Header component here
-//
+import React from 'react';
+
+import '../styles/Header.css';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="main-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,34 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        {movies.map((movie, i) => (
+          <MovieCard
+            key={ i }
+            movie={ movie }
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired,
+};
+
+export default MovieList;
