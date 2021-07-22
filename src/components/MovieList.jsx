4b5b9d005abf505bd0da09dc13d2097cc748d8b3
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-22 16:21:17 -0300

    Implementa nova class MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>List</ul>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
