a044a578eef8dc219763e8de5f812c62bc935084
Alex <alexcorreas1994@gmail.com>
2021-07-22 18:53:26 -0300

    Estilizando os componentes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,12 +7,14 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <section>
-        <img src={ imagePath } alt="" />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
-        <Rating rating={ rating } />
+      <section className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{title}</h4>
+          <h5 className="movie-card-subtitle">{subtitle}</h5>
+          <p className="movie-card-storyline">{storyline}</p>
+          <Rating rating={ rating } />
+        </div>
       </section>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,8 +5,10 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <section className="rating">
-        { rating }
+      <section className="movie-card-rating">
+        <div className="rating">
+          { rating }
+        </div>
       </section>
     );
   }
