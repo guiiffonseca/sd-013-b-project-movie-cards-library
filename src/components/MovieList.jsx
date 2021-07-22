7bb8509f3fd13bf47ac96ce6f6d0694ef371fb1c
Henrique Cursino <henriquecursino46@gmail.com>
2021-07-22 16:05:47 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+
+import React, { Component } from 'react';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <h1>teste</h1>
+    );
+  }
+}
+
+export default MovieList;
