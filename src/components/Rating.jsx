36ad638e6471dd33eb2b9c4e80522429b961db6b
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-22 15:54:50 -0300

    corrigindo props

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React from 'react';
+import PropType from 'prop-types';
 
 function Rating(props) {
   const { rating } = props;
   return <p className="rating">{rating}</p>;
 }
+
+Rating.propTypes = {
+  rating: PropType.number.isRequired,
+};
 
 export default Rating;
