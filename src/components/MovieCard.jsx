9b1006af961e922831ce85c48f14ae93b17401cb
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-22 16:40:58 -0300

    Requisitos 15 e 16 - App Component e CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,21 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <div>
-        <img src={ movie.imagePath } alt="" />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="movie" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating className="movie-card-rating" rating={ rating } />
+        </div>
       </div>
     );
   }
