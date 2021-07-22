97f20786510b99734f07c276d2a63ae9c7f3fca9
Jacson Rodrigues <jacson.s.r1@gmail.com>
2021-07-22 15:42:10 -0300

    Adds Requirement 1 & 2.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import './App.css';
+
 import Header from './components/Header';
+
+import './App.css';
+import movieData from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movieData={ movieData } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,40 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movieData } = this.props;
+
+    return (
+      <ul className="movie-list">
+        {movieData.map((movie, index) => (
+          <li className="movie-card movie-card-body" key={ index }>
+            <img
+              src={ movie.imagePath }
+              alt={ movie.title }
+              className="movie-card-image"
+            />
+            <h2 className="movie-card-title">{ movie.title }</h2>
+            <h3 className="movie-card-subtitle">{ movie.subtitle }</h3>
+            <p className="movie-card-storyline">{ movie.storyline }</p>
+            <div className="movie-card-rating">
+              <span className="rating">{movie.rating}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movieData: propTypes.arrayOf({
+    title: propTypes.string.isRequired,
+    subtitle: propTypes.string.isRequired,
+    storyline: propTypes.string.isRequired,
+    rating: propTypes.number.isRequired,
+    imagePath: propTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default MovieList;
