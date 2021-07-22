511dd4cfc311840bfda405d8e59815ce2f4e31d6
eaeniko <eaenikolai@gmail.com>
2021-07-22 19:34:37 -0300

    [Nikolai] css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <header>
-        <h1 className="movie-card-header">Movie Cards Library</h1>
+        <h1 className="movie-card-header page-title">Movie Cards Library</h1>
       </header>
     );
   }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,11 +7,11 @@ class MovieCard extends Component {
     const { movie } = this.props;
     const { title, imagePath, subtitle, storyline, rating } = movie;
     return (
-      <div className="movie-card">
-        <h4>{ title }</h4>
-        <img src={ imagePath } alt={ title } />
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+      <div className="movie-card movie-card-body">
+        <h4 className="movie-card-title">{ title }</h4>
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
         <Rating rating={ rating } />
       </div>
     );
