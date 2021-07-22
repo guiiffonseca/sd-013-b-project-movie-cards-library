babd974c8d2a53765eb316076eec7b09393d0e32
Alexandre Altgauzen <altgauzen@gmail.com>
2021-07-22 22:48:08 +0100

    requisitos 1 a 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,24 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return (
+      <section className='movie-card'>
+        <h1>
+          {title}
+          {subtitle}
+        </h1>
+        <p>{storyline}</p>
+        
+        <p>{rating}</p>
+
+
+
+      </section>
+    )
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,36 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section>
+        { movies.map(({ title, subtitle }) => (
+          <MovieCard
+            key={ title }
+            movie={ subtitle }
+          />
+        ))}
+      </section>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        imagePath: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+      },
+    ),
+  ).isRequired,
+};
+
+export default MovieList;
