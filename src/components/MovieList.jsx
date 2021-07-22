cd216266f977f98677287bd75ad38111c43fc498
alanmdf <alanmoreirascr@gmail.com>
2021-07-22 15:00:26 -0300

    Requisito 1, 2 e 3 concluidos

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import movies from '../data';
+
+class MovieList extends Component {
+  render() {
+    return (
+      {movies.map((movie, index) => {
+        <MovieList 
+        key={ index }
+        />
+      })}
+    )
+  }
+}
+
+export default MovieList;
