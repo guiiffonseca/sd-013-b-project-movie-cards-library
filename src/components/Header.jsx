9286669fc59531f157941730722595dd256131af
guiiffonseca <guii.ffonseca22@gmail.com>
2021-07-22 15:09:53 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
-
+import React from 'react';
 // implement Header component here
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
