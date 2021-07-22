3883d451ac91712858d355e3b3fed1f0899e925d
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-22 16:47:38 -0300

    Adiciona requisitos 3 e 16

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,28 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div>
+        <p>{ title }</p>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+        <img src={ imagePath } alt="imagem" />
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
+export default MovieList;
