42ff10d1653c0b32cf445d8366f59ead49740227
diogodlemos <diogodrummond@hotmail.com>
2021-07-22 15:18:42 -0300

    Criando o componente <Header /> para renderizar o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,13 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <footer className="movie-card-header">
+        <h1 className="page-title">Movie Cards Library</h1>
+      </footer>
+    );
+  }
+}
+
+export default Header;
