9f64376921c94951fb58416b9f5ac0e3405252ff
Mariana Garcia <maridgarcia045@gmail.com>
2021-07-22 20:03:45 -0300

    Corrige linhas

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,10 +12,10 @@ class MovieCard extends React.Component {
         <h4 className="movie-card-title">{ title }</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
-          <div className="movie-card-rating">
+        <div className="movie-card-rating">
         Rating
-          <Rating rating={ rating } />
-        </div>
+            <Rating rating={ rating } />
+          </div>
       </div>
     );
   }
