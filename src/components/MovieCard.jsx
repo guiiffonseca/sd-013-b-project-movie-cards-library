3d799a16ad2768a26401c95216d5d889944cc420
btriz <00btrz@gmail.com>
2021-07-22 18:08:24 -0300

    até requisito 10

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,9 +8,12 @@ class MovieCard extends React.Component {
 
     return (
       <li>
-        { title }
-        { subtitle }
-        { storyline }
+        <div>
+          <img src={ imagePath } alt={ `imagem do filme ${title}` } />
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+        </div>
       </li>
     );
   }
