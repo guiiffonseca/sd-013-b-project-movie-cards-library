4868457d78654bed7c9cf7325320e161a5ded4b8
Alexandre Altgauzen <altgauzen@gmail.com>
2021-07-22 23:22:30 +0100

    requisitos 1 a 6 refatorado 1.1

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import movies from '../data';
 
 class Rating extends React.Component {
   render() {
     return (
-      
-    )
+      <div className="rating">
+        { movies.map(({ title, rating }) => (
+          <Rating
+            key={ title }
+            rating={ rating }
+          />
+        ))}
+      </div>
+    );
   }
 }
+
+Rating.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        imagePath: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+      },
+    ),
+  ).isRequired,
+};
 
 export default Rating;
