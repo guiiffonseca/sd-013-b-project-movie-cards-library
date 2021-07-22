e8d0d699699ab0ce1ef96b16feb71436082f8bc6
Amós Rodrigues <amos.adm.rh@gmail.com>
2021-07-22 16:50:50 -0300

    Realiza requistos de 1 ao 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,14 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,40 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div>
+        { movies.map(({ title, subtitle, storyline, rating, imagePath }) => (
+          <MovieCard
+            key={ title }
+            title={ title }
+            subtitle={ subtitle }
+            rating={ rating }
+            storyline={ storyline }
+            imagePath={ imagePath }
+          />
+        )) }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
+};
+
+export default MovieList;
