8f255888d14d7a3f8877a701d49d2f6431e67753
Alex <alexcorreas1994@gmail.com>
2021-07-22 17:32:51 -0300

    Renderizar a nota de um filme dentro de Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <h1>aa</h1>
+      <section className="rating">
+        { rating }
+      </section>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
