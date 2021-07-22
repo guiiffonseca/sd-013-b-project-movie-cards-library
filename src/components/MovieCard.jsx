eb022b8c47ce7b93731e3a6b539d8a5839d6b5c6
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-07-22 16:37:08 -0300

    desafios 1 ao 4

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class  MovieCard extends React.Component {
+    render() {
+        const { key, title, subtitle, storyline, rating, imagePath} = this.props;
+        return(
+            <h1>teste</h1>
+        )
+    }
+}
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,23 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+    render() {
+        const { movies } = this.props;
+        return (
+            movies.map(({title, subtitle, storyline, rating, imagePath }, index) => (
+                <MovieCard 
+                key={index}
+                title={title}
+                subtitle={subtitle}
+                storyline={storyline}
+                rating={rating}
+                imagePath={imagePath}
+                />
+            ))
+            
+        );
+    }
+}
+
+export default MovieList;
