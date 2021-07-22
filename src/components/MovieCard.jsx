a214ffdcd6818e922a6426ba1a231fd88d3ef7b9
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-22 18:41:35 -0300

    Refatora o CSS.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,16 +6,14 @@ class MovieCart extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div className="movie-list">
-        <div className="movie-card">
-          <div className="movie-card-body">
-            <h4 className="movie-card-title">{ movie.title }</h4>
-            <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
-            <p className="movie-card-storyline">{ movie.storyline }</p>
-          </div>
-          <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
-          <Rating rating={ movie.rating } />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
         </div>
+        <img className="movie-card-image" src={ movie.imagePath } alt={ movie.title } />
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
