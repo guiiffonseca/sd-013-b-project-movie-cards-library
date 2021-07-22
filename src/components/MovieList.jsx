dc5f10c8d04a8b1813be6e10c6d5e4b5983d37b1
PedroSpelta <p.h.s.8989@hotmail.com>
2021-07-22 14:34:12 -0300

    fazendo o req 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,9 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+function MovieList(props) {
+  const { movies } = props;
+  return movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+}
+
+export default MovieList;
