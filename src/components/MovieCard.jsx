8b1555655319f11c8cb653b4414e5903cb67f14a
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-07-22 16:00:36 -0300

    Requisito 3, 4 e 5 Finalizados sem conflitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,11 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <h1>a</h1>
+    );
+  }
+}
+
+export default MovieCard;
