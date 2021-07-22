9027578cbbdc31c4f34ab3832a43c5d0e01e888b
Henrique Cursino <henriquecursino46@gmail.com>
2021-07-22 18:12:20 -0300

    Finalizando projeto

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,12 @@
 // implement MovieList component here
 
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends Component {
   render() {
+    const { movies } = this.props;
     return (
       <div className="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
@@ -13,5 +14,9 @@ class MovieList extends Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
