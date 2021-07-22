5d12ce28c7705477c521395c3c4bf72101745133
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-22 17:39:26 -0300

    Implementa lógica para o componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,21 @@
 // implement MovieList component here
 import React, { Component } from 'react';
+import movies from '../data';
 
 class MovieList extends Component {
   render() {
+    // const { title, subtitle, storyline, rating, imagePath } = movies;
     return (
       <div>
-        <ul>List</ul>
+        { movies.map(({ title, subtitle, storyline, rating, imagePath }, index) => (
+          <section key={ index }>
+            <img source={ imagePath } alt="Movie illustration.png" />
+            <h3>{title}</h3>
+            <h2>{subtitle}</h2>
+            <p>{storyline}</p>
+            <div>{rating}</div>
+          </section>
+        )) }
       </div>
     );
   }
