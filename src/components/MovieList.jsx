a6052319e9872a88b186606535f1136d45e7a88e
EddieRAM <edusacarneiro@gmail.com>
2021-07-22 18:50:56 -0300

    Requiste 4 done

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section className="movies">
+        <div>
+          {movies}
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MovieList;
