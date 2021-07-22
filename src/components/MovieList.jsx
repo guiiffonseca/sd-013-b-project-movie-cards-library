44cf7b3c24e62de69dd24e3da4301331308a4c1f
Alex <alexcorreas1994@gmail.com>
2021-07-22 17:17:56 -0300

    Renderizando componente de MovieCard dentro de MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <h1>aa</h1>
+      <main>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
