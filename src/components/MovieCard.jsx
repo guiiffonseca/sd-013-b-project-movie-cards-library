bdb971c8e3f2a47fdd4426f3fb0166af3aa9ab77
FilipeGuto <filipeguto95@gmail.com>
2021-07-22 19:33:59 -0300

    Organizaçao CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,14 +6,16 @@ class MovieCard extends Component {
   render() {
     const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
-      <div>
-        <img src={ imagePath } alt={ title } />
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <p>{ storyline }</p>
-        <Rating
-          rating={ rating }
-        />
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <img className="movie-card-image" src={ imagePath } alt={ title } />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <Rating
+            rating={ rating }
+          />
+        </div>
       </div>
     );
   }
@@ -21,11 +23,11 @@ class MovieCard extends Component {
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    imagePath: PropTypes.string.isRequired,
-    subtitle: PropTypes.string.isRequired,
-    storyline: PropTypes.string.isRequired,
-    rating: PropTypes.number.isRequired,
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
