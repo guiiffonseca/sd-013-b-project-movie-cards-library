5e127d1ec2801ecf2e66254187e11524eb0c0c04
vitor hugo da silva <vitorhugo.dev1997@gmail.com>
2021-07-22 17:45:20 -0300

    desafio 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 class  MovieCard extends React.Component {
     render() {
-        const { key, title, subtitle, storyline, rating, imagePath} = this.props;
-        return(
-            <h1>teste</h1>
+        const { title, subtitle, storyline, rating, imagePath} = this.props;
+        return(           
+                <h1> teste </h1>           
         )
     }
 }
+
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import PropTypes from 'prop-types';
 
 class MovieList extends React.Component {
-    render() {
-        const { movies } = this.props;
-        return (
-            movies.map(({title, subtitle, storyline, rating, imagePath }, index) => (
-                <MovieCard 
-                key={index}
-                title={title}
-                subtitle={subtitle}
-                storyline={storyline}
-                rating={rating}
-                imagePath={imagePath}
-                />
-            ))
-            
-        );
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className='movie-list'>
+        {movies.map(({ title, subtitle, storyline, rating, imagePath }) => (
+          <MovieCard
+            key={title}
+            title={title}
+            subtitle={subtitle}
+            storyline={storyline}
+            rating={rating}
+            imagePath={imagePath}
+          />
+        ))}
+      </section>
+    );
+  }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default MovieList;
