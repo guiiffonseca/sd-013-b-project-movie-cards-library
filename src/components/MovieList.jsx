6a1f0623936e8eff03918bf43cf4bfde2e09a102
bvottri <bruna.votre@gmail.com>
2021-07-22 15:47:52 -0300

    Requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+// import movies from './tests/data.js';
 
 // const movies = ['Star Wars', 'Shrek', 'Up! Altas Aventuras'];
 
 class MovieList extends React.Component {
   render() {
     return (
-      <div>{props.movies}</div>
+      <div>oi</div>
     );
   }
 }
