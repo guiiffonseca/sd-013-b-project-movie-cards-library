df8a341a44b63abd764717cb5c47d207c5f445fe
diogodlemos <diogodrummond@hotmail.com>
2021-07-22 18:17:21 -0300

    Requisito 3 finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Movies from './data';
+import MovieList from './components/MovieList';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <MovieList movies={ Movies } />
     </div>
   );
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,37 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        { movies.map(({ title, subtitle, storyline, rating, imagePath }) => (
+          <div className="movie-card" key={ title }>
+            <img className="movie-card-image" src={ imagePath } alt={ title } />
+            <h2 className="movie-card-title">{ title }</h2>
+            <h5 className="movie-card-subtitle">{ subtitle }</h5>
+            <p className="movie-card-storyline">{ storyline }</p>
+            <p className="rating">{ rating }</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        rating: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
+};
+
+export default MovieList;
