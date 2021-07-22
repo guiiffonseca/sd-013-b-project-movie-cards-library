65bab4414db13c51289a4cbd9c03a17dafc0482e
marcus <marcus.rodrigues10@hotmail.com>
2021-07-22 15:27:43 -0300

    commitao bomba

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,23 +1,21 @@
 // implement MovieList component here
 import React from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
-class MovieList extends React.Component {
+export default class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div>
-        { movies.map((movie) => (
-          <MovieCard 
-          key= { movie.title }
-          movie={ movie }
-          />
-
+        {movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />
         ))}
       </div>
     );
   }
 }
 
-export default MovieList;
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
