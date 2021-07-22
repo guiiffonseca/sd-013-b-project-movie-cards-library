ac1f8940dbbcc3e059bcd858ac3486c6b0138045
btriz <00btrz@gmail.com>
2021-07-22 17:47:19 -0300

    até requisito 6

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
       <main>
-        a
+        <ul>
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </ul>
       </main>
     );
   }
 }
 
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 export default MovieList;
