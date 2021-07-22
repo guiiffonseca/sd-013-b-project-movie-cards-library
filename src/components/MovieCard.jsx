2c113764aea0a1f2b60083f1f35340307b13c216
Felipe Ventorim <felipeventorim11@gmail.com>
2021-07-22 17:08:22 -0300

    CSS

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -14,7 +14,9 @@ class MovieCard extends React.Component {
           <h4 className="movie-card-title">{ title }</h4>
           <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
-          <Rating className="movie-card-rating" rating={ rating } />
+          <div className="movie-card-rating">
+            <Rating rating={ rating } />
+          </div>
         </div>
       </div>
     );
