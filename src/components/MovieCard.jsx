96eaf1a0c9c6b1642654e5cb938bb64f06c6d2fd
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-07-22 15:12:10 -0300

    test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      <div>
+        <img src={ this.props.imagePath } alt="" />
+        <h4>{this.props.title}</h4>
+        <h5>{this.props.subtitle}</h5>
+        <p>{this.props.storyline}</p>
+        <Rating />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  imagePath: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main>
+        {movies.map(({ imagePath, title, subtitle, storyline, rating }, index) => (
+          <MovieCard
+            key={ index }
+            imagePath={ imagePath }
+            title={ title }
+            subtitle={ subtitle }
+            storyline={ storyline }
+            rating={ rating }
+          />
+        ))}
+      </main>
+    );
+  }
+}
+
+export default MovieList;
