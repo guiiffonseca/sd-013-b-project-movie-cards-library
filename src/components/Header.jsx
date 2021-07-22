36626b59d7fa7175ec2978bc9adae5285ade68db
alanmdf <alanmoreirascr@gmail.com>
2021-07-22 16:58:32 -0300

    Projeto finalizado, lint ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ class Header extends Component {
       <header>
         <h1>Movie Cards Library</h1>
       </header>
-    )
+    );
   }
 }
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className='rating'>{ rating }</div>
-    )
+      <div className="rating">{ rating }</div>
+    );
   }
 }
+
+Rating.defaultProps = {
+  rating: 5,
+};
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
 
 export default Rating;
