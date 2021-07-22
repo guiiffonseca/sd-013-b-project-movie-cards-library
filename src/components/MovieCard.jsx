309bf773928af0137e515f38e7da538302674097
Rafael de Oliveira Romano <rafa-kun@hotmail.com>
2021-07-22 17:21:03 -0300

    aadicionando os 100%

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,7 @@
 // implement MovieCard component here
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
@@ -11,12 +12,14 @@ class MovieCard extends Component {
         <h4>{movie.title}</h4>
         <h5>{movie.subtitle}</h5>
         <p>{movie.storyline}</p>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
 }
 MovieCard.propTypes = {
   movie: PropTypes.string.isRequired,
+  rating: PropTypes.string.isRequired,
 };
 
 export default MovieCard;
