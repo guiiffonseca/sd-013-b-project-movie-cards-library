cdaf40fc0ec9dc9561d53a73e2109c37820286fe
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-22 15:55:59 -0300

    Requisito 6: Renderiza MovieCard com PropTypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     return <div>MovieCard</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
