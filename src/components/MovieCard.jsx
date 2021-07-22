7e0ce99d8fa4ba814972ce52337edb64a4837664
eaeniko <eaenikolai@gmail.com>
2021-07-22 18:49:21 -0300

    [Nikolai] Requisito  3-4-5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
+
 class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+    const { title, imagePath, subtitle, storyline, rating } = movie;
     return (
-      <p>a</p>
+      <div className="movie-card">
+        <p>{ title }</p>
+        <img src={ imagePath } alt={ title } />
+        <p>{subtitle}</p>
+        {storyline}
+        {rating}
+      </div>
     );
   }
 }
