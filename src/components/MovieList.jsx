c0fbff45ab9109e4c04df0230f20d34528937da1
Yg.Or <inygu@icloud.com>
2021-07-22 15:18:55 -0300

    requisito 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return <h1>{movies}</h1>;
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default MovieList;
