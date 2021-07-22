ff066010689a4a41b5f83c66ffab4bfeb4ac3ddc
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-07-22 20:55:14 -0300

    itens 1 a 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+
+import React from 'react';
+import movies from '../data';
+/* import PropTypes from 'prop-types'; */
+
+class MovieList extends React.Component {
+  render() {
+    return movies;
+  }
+}
+
+export default MovieList;
