880130159cc409ff39da4e9c82297f4e605f134b
bvottri <bruna.votre@gmail.com>
2021-07-22 15:32:50 -0300

    Requisito 1 e 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// implement MovieList component here
+import React from 'react';
+
+// const movies = ['Star Wars', 'Shrek', 'Up! Altas Aventuras'];
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>{props.movies}</div>
+    );
+  }
+}
+
+export default MovieList;
