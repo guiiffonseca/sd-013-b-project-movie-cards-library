2e203b2b952310aba5e6cbe2e4e89c84053146ce
Joao Victor <joaogmavictor@gmail.com>
2021-07-22 15:26:15 -0300

    Concluindo Movie Card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,32 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+
+class MovieCard extends Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props;
+    return (
+      <section>
+        <img src={ imagePath } alt="" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  imagePath: PropTypes.string,
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+  storyline: PropTypes.string,
+};
+
+MovieCard.defaultProps = {
+  imagePath: 'images/movie_3',
+  title: 'Movie Title 1',
+  subtitle: 'Movie Subtitle 1',
+  storyline: 'Movie Storyline 1',
+};
+
+export default MovieCard;
