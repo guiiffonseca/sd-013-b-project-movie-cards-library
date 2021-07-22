3c3f942c52f7f201a4ed401c5cdd1fae69fac176
FilipeGuto <filipeguto95@gmail.com>
2021-07-22 18:53:21 -0300

    all Exercises

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { movie } = this.props;
-    const { imagePath, title, subtitle, storyline, rating } = movie;
+    const { movie: { imagePath, title, subtitle, storyline, rating } } = this.props;
     return (
       <div>
         <img src={ imagePath } alt={ title } />
         <h4>{ title }</h4>
         <h5>{ subtitle }</h5>
         <p>{ storyline }</p>
-        <p>{ rating }</p>
+        <Rating
+          rating={ rating }
+        />
       </div>
     );
   }
