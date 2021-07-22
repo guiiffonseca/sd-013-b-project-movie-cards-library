8c08c79861f541ae52b2cad0a20e221f17f64178
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-22 16:03:03 -0300

    Adiciona requisitos 1 e 15

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,11 @@
-// implement Header component here
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>Movie Cards</header>
+    );
+  }
+}
+
+export default Header;
