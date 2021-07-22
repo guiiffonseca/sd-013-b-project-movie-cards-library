23496d767a1c87d8cf99e612cc288435df89bfaf
wendeldemelo <demelowendel@gmail.com>
2021-07-22 18:32:19 -0400

    requisio 1 e 2 finalizado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,1 +1,19 @@
-// implement Header component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Header extends React.Component {
+  render() {
+    const { header } = this.props;
+    return (
+      <header className="movie-card-header">
+        <h1>Movie Cards Library</h1>
+      </header>
+    );
+  }
+}
+
+Header.propTypes = {
+  header: PropTypes.string.isRequired,
+};
+
+export default Header;
