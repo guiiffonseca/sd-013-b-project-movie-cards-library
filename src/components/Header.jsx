e8437da280019ed5133bcde00f5a1ccbcad575d1
Polyana Balotin <polybalotin@hotmail.com>
2021-07-22 16:29:13 -0300

    requisito 2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
-import '../App.css';
 import React from 'react';
 
 class Header extends React.Component {
   render() {
     return (
       <header className="movie-card-header">
-        <h1 className="page-title">Movie Cards</h1>
+        <h1 className="page-title">Movie Cards Library</h1>
       </header>
     );
   }
