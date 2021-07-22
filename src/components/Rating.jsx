cc112038026284214ca8278e322a4ddff0e0a17d
Joao Victor <joaogmavictor@gmail.com>
2021-07-22 15:31:52 -0300

    Tirando Erros de Lint Para rodar Teste no Remoto

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,23 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    const { ratingNumber } = this.props;
+    return (
+      <div>
+        <p>{ratingNumber}</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  ratingNumber: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  ratingNumber: 3,
+};
+
+export default Rating;
