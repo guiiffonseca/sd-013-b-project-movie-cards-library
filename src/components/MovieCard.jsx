95825c004422ddfa6d122bb7b596af15545dfe50
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-07-22 19:26:21 -0300

    Outro comitão bomba

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,28 +1,32 @@
 // implement MovieCard component here
 import React from 'react';
-import movies from '../data';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { title, subtitle, storyline, imagePath } = this.props;
     return (
-      movies.map(({ title, subtitle, storyline, imagePath }, index) => {
-        return (
-          <section key={ index } className="movie-card">
-            <img
-              src={ imagePath }
-              alt={ `Imagem do filme ${title}` }
-              className="movie-card-image"
-            />
-            <div className="movie-card-body">
-              <h2 className="movie-card-title">{ title }</h2>
-              <h3 className="movie-card-subtitle">{ subtitle }</h3>
-              <p className="movie-card-storyline">{ storyline }</p>
-            </div>
-          </section>
-        );
-      })
+      <section key={ title } className="movie-card">
+        <img
+          src={ imagePath }
+          alt={ `Imagem do filme ${title}` }
+          className="movie-card-image"
+        />
+        <div className="movie-card-body">
+          <h2 className="movie-card-title">{ title }</h2>
+          <h3 className="movie-card-subtitle">{ subtitle }</h3>
+          <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+      </section>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
 
 export default MovieCard;
