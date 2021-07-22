1bd7e43415505f557a4b13702fa91f1c3132022b
Joao Victor Cristino F <jvictorfranca@yahoo.com.br>
2021-07-22 15:41:58 -0300

    Vai arrumar o lint ne?

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,7 +18,14 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
 
+  }).isRequired,
 };
 
 export default MovieCard;
