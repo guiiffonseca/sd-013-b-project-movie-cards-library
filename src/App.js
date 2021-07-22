810d38ce5b1783bd8e44b631aad44191e5ddda54
Thaís Sampaio <thais_castrocs@outlook.com>
2021-07-22 17:48:20 -0300

    Requisito 3 Realizado

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 
-// Retornando o Header dentro da dunção App;
+// Retornando o Header dentro da função App;
 function App() {
   return <Header />;
 }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,22 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// Requisito 3
+
+class MoveList extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <main>
+        {movie}
+      </main>
+    );
+  }
+}
+
+// Verifica-se os dados recebidos são válidos
+MoveList.propTypes = {
+  movie: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MoveList;
