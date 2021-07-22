0f3afebd068e0c102a00c0baf16b900fbb55dbf0
Matheus Figueiredo <mathhsoares2@live.com>
2021-07-22 15:36:19 -0300

    Requisito 3: Cria componente MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,21 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    const p = this.props;
+    const list = p.movies;
+    return (
+      <div>
+        MovieList:
+        {list}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default MovieList;
