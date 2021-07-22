296a17b60b258e4b613ba7700410cd631cdc7921
eaeniko <eaenikolai@gmail.com>
2021-07-22 15:48:56 -0300

    [Nikolai] Requisito 1-2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1 className="movie-card-header">Movie Cards Library</h1>
+      </header>
+    )}
 
 }
 
