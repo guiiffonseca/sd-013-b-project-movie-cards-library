d999b0cd129d53caeff0c417f36bd60a354e9085
Joao Victor <joaogmavictor@gmail.com>
2021-07-22 15:26:23 -0300

    Concluindo Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,40 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const movies = [
+      {
+        title: 'Movie Title 1',
+        subtitle: 'Movie Subtitle 1',
+        storyline: 'Movie Storyline 1',
+        rating,
+        imagePath: 'images/movie_1',
+      },
+      {
+        title: 'Movie Title 2',
+        subtitle: 'Movie Subtitle 2',
+        storyline: 'Movie Storyline 2',
+        rating,
+        imagePath: 'images/movie_2',
+      },
+      {
+        title: 'Movie Title 3',
+        subtitle: 'Movie Subtitle 3',
+        storyline: 'Movie Storyline 3',
+        ratingThree,
+        imagePath: 'images/movie_3',
+      },
+    ];
+    return (
+      <div>
+        {movies.map((movie, index) => (
+          <MovieCard Movie={ movie } key={ index } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
