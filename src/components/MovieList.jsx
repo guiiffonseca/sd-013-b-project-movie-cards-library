f5c5f84514c813143b5662e4bccd557963caec47
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-22 17:56:52 -0300

    Importa componente MovieCard em MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,7 @@
 // implement MovieList component here
 import React, { Component } from 'react';
 import movies from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends Component {
   render() {
