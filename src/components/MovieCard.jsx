ee7db88a3f93e79e0a703bf6bbcf71a673bc1b68
marcus <marcus.rodrigues10@hotmail.com>
2021-07-22 15:02:01 -0300

    exercise 3

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
 // implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+
+  render() {
+    const { title, subtitle, storyline, imagePath, rating } = this.props;
+    return(
+      <div>
+        <img src= { imagePath } />
+        <h4> { title } </h4>
+        <h5></h5>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,21 @@
 // implement MovieList component here
 import React from 'react';
 import movies from '../data';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      { movies }
+      <div>
+        { movies.map((movie) => (
+          <MovieCard 
+          key= { movie.title }
+          movie={ movie }
+          />
+
+        ))}
+      </div>
     );
   }
 }
