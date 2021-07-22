86da99e56bf54bd693346a2281738e2f02f6e059
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-22 16:24:32 -0300

    Adiciona requisitos 2: Tinha esquecido do h1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 class Header extends Component {
   render() {
     return (
-      <header>Movie Cards Library</header>
+      <h1>
+        <header>Movie Cards Library</header>
+      </h1>
     );
   }
 }
