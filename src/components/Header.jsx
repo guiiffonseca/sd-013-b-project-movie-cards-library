b0f956d816e4bb673807a442aeb4910a9be63af7
Alef Sloan <Alef_sloan@hotmail.com>
2021-07-22 16:25:36 -0300

    Requisitos 1 e 2 corrigidos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 class Header extends React.Component {
   render() {
-    const { title } = this.props;
     return (
       <header>
-        <h1>{title}</h1>
+        <h1>Movie Cards Library</h1>
       </header>
     );
   }
 }
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-
 export default Header;
