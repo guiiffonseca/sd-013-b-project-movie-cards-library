0381e47f0385e21744c55d768e3adfb10e109847
Rodrigo Marchi Silva <rodrigomarchisilva@gmail.com>
2021-07-22 17:38:26 -0300

    Primeira bomba armada

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,18 @@
-// implement MovieCard component here
+// implement MovieList component here
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    return (
+      <div>
+        <img src={ this.props.movie.imagePath } alt="Movie Scene" />
+        <h4>{ this.props.movie.title }</h4>
+        <h5>{ this.props.movie.subtitle}</h5>
+        <p>{ this.props.movie.storyline}</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
 // implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.movies.map((movie, index) => (
+            <MovieCard key={ index } movie={ movie } />
+          ))
+        }
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default MovieList;
