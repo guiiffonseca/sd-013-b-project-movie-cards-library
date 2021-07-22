0d1b7bc8b7d7a96ce7b820e64085ea9d98b48da5
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-22 16:51:49 -0300

    Refatora o terceiro e quarto Requisito.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class MovieCart extends React.Component {
   render() {
+    const { data } = this.props;
     return (
-      <h1>{this.props.data.subtitle}</h1>
-    )
+      <h1>{data.subtitle}</h1>
+    );
   }
 }
+
+MovieCart.propTypes = {
+  data: PropTypes.shape(
+    {
+      title: PropTypes.string.isRequired,
+      subtitle: PropTypes.string.isRequired,
+      storyline: PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired,
+      imagePath: PropTypes.string.isRequired,
+    },
+  ).isRequired,
+};
 
 export default MovieCart;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCart from './MovieCard';
 
 class MovieList extends React.Component {
-  render () {
+  render() {
+    const { movies } = this.props;
     return (
-      this.props.movies.map((movie) => (
-        <MovieCart data={ movie }/>
+      movies.map((movie, index) => (
+        <MovieCart key={ index } data={ movie } />
       ))
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
+};
 
 export default MovieList;
