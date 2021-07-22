cbb207bebf7488e7303ef299d9abbf5152143560
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-22 15:52:27 -0300

    requisito 3 - concluido

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,24 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <main className='movie-list'>
+        Lista de filmes
+      </main>
+    );
+  };
+};
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  })),
+};
+
+export default MovieList;
