63db50b1738cd1dac0db2e79b527367662cfe7d9
André <andrebpessoa09@gmail.com>
2021-07-22 19:01:51 -0300

    Fix

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,12 @@
 // implement MovieList component here
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
+
     return (
       <section className="movie-list">
         {movies.map((movie) => (<MovieCard key={ movie.title } movie={ movie } />))}
@@ -12,5 +14,9 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
 
 export default MovieList;
