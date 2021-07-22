c23cdf9a5545dd647a9d56930e14c45343bc8e9b
Jeferson Oliveira <jeferson.contas@gmail.com>
2021-07-22 18:01:58 -0300

    Requi. 6-12

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,10 +7,10 @@ class MovieCard extends React.Component {
     return (
       <ul>
         <img src={ imagePath } alt={ title } />
-        <li>{title}</li>
-        <li>{subtitle}</li>
-        <li>{storyline}</li>
-        <li>{rating}</li>
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+        <Rating rating={ rating } />
       </ul>
     );
   }
