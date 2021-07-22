6b2d07d5b19f04c5e024660cf0e12d873b5c5bb1
Rafael Nery Machado <rafalenerymachado@gmail.com>
2021-07-22 16:39:12 -0300

    corrigido lint

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,19 +5,26 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    const {
+      movie: {
+        title,
+        subtitle,
+        storyline,
+        rating,
+        imagePath,
+      } } = this.props;
 
     return (
       <section className="movie-card">
         <img src={ imagePath } alt={ title } className="movie-card-image" />
         <div className="movie-card-body">
-          <h4 className='movie-card-title'>{ title }</h4>
-          <h5 className='movie-card-subtitle'>{ subtitle }</h5>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
         </div>
-          <div className="movie-card-rating">
-            <Rating rating={ rating } />
-          </div>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </section>
     );
   }
