40539c7b0734a2c8ce2f168441295bd168a4eff5
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-22 15:38:57 -0300

    Cria componente Header que retorna um elemento HTML o nó header e uma child com título.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
 // implement Header component here
-//
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h2> Movie Cards </h2>
+      </header>
+    );
+  }
+}
+
+export default Header;
