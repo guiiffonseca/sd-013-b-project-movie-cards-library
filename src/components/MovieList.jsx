aaad09669373b654ea637a1fd03f4ea205bc4105
marcus <marcus.rodrigues10@hotmail.com>
2021-07-22 14:47:29 -0300

    exercise 3

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
 // implement MovieList component here
+import React from 'react';
+import movies from '../data';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      { movies }
+    );
+  }
+}
+
+export default MovieList;
