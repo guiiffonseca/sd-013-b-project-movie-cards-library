62018059a1e0a76754b3574eace5b9ded65b0ee2
Alexsander-Trybe <Alexsander81002745@gmail.com>
2021-07-22 20:10:06 -0300

    Super commitão bomba plus master final

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 import data from './data';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
       <Header />
       <MovieList movies={ data } />
+      <Rating />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,12 @@
 // implement MovieCard component here
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, imagePath } = this.props;
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <section key={ title } className="movie-card">
         <img
@@ -13,9 +15,12 @@ class MovieCard extends React.Component {
           className="movie-card-image"
         />
         <div className="movie-card-body">
-          <h2 className="movie-card-title">{ title }</h2>
-          <h3 className="movie-card-subtitle">{ subtitle }</h3>
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
           <p className="movie-card-storyline">{ storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
         </div>
       </section>
     );
@@ -23,10 +28,13 @@ class MovieCard extends React.Component {
 }
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  imagePath: PropTypes.string.isRequired,
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
 };
 
 export default MovieCard;
