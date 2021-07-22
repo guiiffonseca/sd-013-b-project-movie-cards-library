cd44eeb58b773a516b3599720fd4176cd8f16798
Lucas T Franca <dev.lucasteixeira@gmail.com>
2021-07-22 16:41:03 -0300

    Requisito 11, 12 Finalizados sem conflitos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,17 @@
-// implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends React.Component {
+  render() {
+    const { ratingNumber } = this.props;
+    return (
+      <p>{ ratingNumber }</p>
+    );
+  }
+}
+
+Rating.propTypes = {
+  ratingNumber: PropTypes.number.isRequired,
+};
+
+export default Rating;
