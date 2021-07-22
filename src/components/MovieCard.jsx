543667e8b905e3dee6f7e89209bc8eeb84c79c8f
Arthur-Jr <arthurjr1906@gmail.com>
2021-07-22 16:26:20 -0300

    Adiciona o terceiro e quarto Requisito.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,2 +1,11 @@
-// implement MovieCard component here
-//
+import React from 'react';
+
+class MovieCart extends React.Component {
+  render() {
+    return (
+      <h1>{this.props.data.subtitle}</h1>
+    )
+  }
+}
+
+export default MovieCart;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,14 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCart from './MovieCard';
+
+class MovieList extends React.Component {
+  render () {
+    return (
+      this.props.movies.map((movie) => (
+        <MovieCart data={ movie }/>
+      ))
+    );
+  }
+}
+
+export default MovieList;
