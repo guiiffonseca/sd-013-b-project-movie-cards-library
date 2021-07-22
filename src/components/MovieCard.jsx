1980d92ab65e15a860926443b7ff4e6056f17b98
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-22 15:40:35 -0300

    resolve a questao sobre o movecard e o movelist, mas precisa solucionar problemas com o propTypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,29 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, imagePath } = this.props.movie;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        {/* <Rating /> */}
+      </div>
+    );
+  }
+}
+
+// MovieCard.propTypes = {
+//   movie: Proptypes.shape({
+//     title: Proptypes.string,
+//     subtitle: Proptypes.string,
+//     storyline: Proptypes.string,
+//     imagePath: Proptypes.string,
+//     rating: Proptypes.number,
+//   }).isRequired,
+// };
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,16 @@
-// implement MovieList component here
+import React from 'react';
+import Proptypes from 'prop-types';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
