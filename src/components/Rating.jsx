2d0040824be4cc6cb34e98261a279cf0f96444a6
Jessica Naiara <jessicanaiarag@gmail.com>
2021-07-23 14:26:09 -0300

    Resoluçao de requisitos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -6,8 +6,8 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <div className="movie-card-rating">
-        <div className="rating">{rating}</div>
+      <div>
+        <div>{rating}</div>
       </div>
     );
   }
