325f4be87f503005de110680dc0ba6361ed051b9
Raphael Rodrigues <raphael_rpc@outlook.com>
2021-07-22 23:30:51 -0300

    refaturacao dos elementos

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -5,8 +5,8 @@ class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-      <section className="rating">
-        <span className="movie-card-rating">{rating}</span>
+      <section className="movie-card-rating">
+        <span className="rating">{ rating }</span>
       </section>
     );
   }
