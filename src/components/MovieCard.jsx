31cabd164125e33d79a20afe4c5beb3eaf65d5b8
Carol_Inglethe <carolinainglethe@gmail.com>
2021-07-22 21:36:25 -0300

    Estilizaçao com classes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,12 +8,14 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
-      <div className="movie-card">
-        <h4>{ title }</h4>
-        <h5>{ subtitle }</h5>
-        <img src={ `${imagePath}` } alt="imagem do filme" />
-        <p>{ storyline }</p>
-        <Rating rating={ rating } />
+      <div className="movie-card movie-card-body">
+        <h4 className="movia-card-title">{ title }</h4>
+        <h5 className="movie-card-subtitle">{ subtitle }</h5>
+        <img className="movie-card-image" src={ `${imagePath}` } alt="imagem do filme" />
+        <p className="movie-card-storyline">{ storyline }</p>
+        <div className="movie-card-rating">
+          <Rating rating={ rating } />
+        </div>
       </div>
     );
   }
