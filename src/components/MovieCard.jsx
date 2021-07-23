6a248552be24ec3c0ce12d93bfe7b2d3707daf76
Gustavo Fernando <gu.bressan@outlook.com>
2021-07-22 17:12:08 -0700

    cara nao sei pq nao ta passando, vou rever o codigo

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
+
 class MovieCard extends React.Component {
   render() {
+    const {key, subtitle, story, image, ratin} = this.props;
     return (
       <div className="movie-card">
-        oi
+        <img src = {image} alt = "Filme"></img>
+        <h4>{key}</h4>
+        <h5>{subtitle}</h5>
+        <p>{story}</p>
+        <p>{ratin}</p>
       </div>
     );
   }
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import MovieCard from './MovieCard';
+import movies from '../data';
+
 
 class MovieList extends React.Component {
   render() {
     return (
-      <div className="movie-list"><MovieCard /></div>
+      <div className="movie-list">
+        { movies.map(({ title, subtitle, storyline, imagePath, rating }) =>
+          <MovieCard
+           key = {title}
+           subtitle = {subtitle}
+           story = {storyline}
+           image = {imagePath}
+           ratin = {rating}
+          />)}
+      </div>
     );
   }
 }
