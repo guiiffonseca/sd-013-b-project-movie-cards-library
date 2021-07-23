3e79c6cc8c8a660eff1fe83d3df270a92821d652
mateusparr <mateus.dji@icloud.com>
2021-07-23 15:04:38 -0300

    requisito 3-8 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt="capa do filme" />
+        <h4>{ movie.title }</h4>
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.node.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <main className="MovieList" />
+      <main className="MovieList">
+        {movies.map((movie, index) => (
+          <MovieCard key={ `Movie Title ${index + 1}` } movie={ movie } />
+        ))}
+        ;
+      </main>
     );
   }
 }
-
+MovieList.propTypes = {
+  movies: PropTypes.node.isRequired,
+};
 export default MovieList;
