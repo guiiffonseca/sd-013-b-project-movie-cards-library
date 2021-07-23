ff306c75567959788ba834b258be2fb7a19d7bbd
Gustavo Fernando <gu.bressan@outlook.com>
2021-07-22 18:21:14 -0700

    lint mais uma vez

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,19 +1,18 @@
 import React from 'react';
-import PropTyper from 'prop-types'
+import PropTyper from 'prop-types';
 
 class Rating extends React.Component {
   render() {
     const { rating } = this.props;
     return (
-    <p className ="rating">
-      { rating }
-    </p>
+      <p className="rating">
+        { rating }
+      </p>
     );
   }
 }
 
 Rating.propTypes = {
-  rating: PropTyper.number.isRequired
-};
+  rating: PropTyper.number.isRequired };
 
 export default Rating;
