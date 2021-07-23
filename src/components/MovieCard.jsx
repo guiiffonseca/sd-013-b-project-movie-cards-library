ecc2d7bafcdb3ba855faa62a79a1054d021491dd
Pedro Sehn <pedrorsehn@hotmail.com>
2021-07-23 09:44:06 -0300

    nao sei quantos requisitos foram cupridos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
-// implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, rating, imagePath } = movie;
+    console.log(this.props);
+    return (
+      <div key={ title } className="movie-card ">
+        <img src={ imagePath } alt="foto do filme" />
+        <h4>
+          { title }
+        </h4>
+        <h5>
+          {subtitle}
+        </h5>
+        <p>
+          {storyline}
+        </p>
+        {rating}
+      </div>);
+  }
+}
+
+export default MovieCard;
+
+MovieCard.shape = ({
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+});
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import MovieCard from './MovieCard';
+
+class MovieList extends Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((crr) => <MovieCard key={ crr.title } movie={ crr } />)}
+      </section>);
+  }
+}
+
+export default MovieList;
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
