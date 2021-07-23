49dac6c8ccba303a43547e90b3f318c7c9ec9ac1
Pedro <pedrorsehn@hotmail.com>
2021-07-23 12:30:44 -0300

    arruma linter

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -25,10 +25,12 @@ class MovieCard extends Component {
 
 export default MovieCard;
 
-MovieCard.shape = ({
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  imagePath: PropTypes.string.isRequired,
-});
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
