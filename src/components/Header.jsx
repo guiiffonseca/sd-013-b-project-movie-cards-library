cee690535a79003d8707290d33ac8184992b17ab
fabio <fabiojuvenalpereira@hotmail.com>
2021-07-23 13:40:08 -0300

    Projeto finalizado :eyes:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,9 +5,9 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <div>
+      <header>
         <h1>Movie Cards Library</h1>
-      </div>
+      </header>
     );
   }
 }
