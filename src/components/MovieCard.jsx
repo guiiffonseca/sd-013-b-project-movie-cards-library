b7b35fe7c175d79d417f07b6814b8c266a59eb73
luis carlos <luizcruz0809@gmail.com>
2021-07-23 10:53:12 -0300

    add 03

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,20 @@
 // implement MovieCard component here
+// import React from 'react';
+
+// class MovieCard extends React.Component {
+//   render() {
+//     const { movie } = this.props;
+
+//     return (
+//       <div>
+//         <h2>{ movie.title }</h2>
+//         <h4>{ movie.subtitle }</h4>
+//         <p>{ movie.storyline }</p>
+//         <div>{ movie.rating }</div>
+//         <img src={ movie.imagePath } alt={ movie.title }/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,17 @@
 // implement MovieList component here
+import React from 'react';
+// import movies from '../data'
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      // movies.map((movie, index) => <MovieCard key={ index } movie={ movie } />)
+      <div>
+        <h1>test</h1>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
