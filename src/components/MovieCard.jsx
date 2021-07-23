662dfdbf1c98eb239b3ac9534277713c1cd290a3
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-23 15:09:52 -0300

    Comitao bomba: Adiciona alguns requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import movies from '../data';
 
 class MovieCard extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    const { movie } = this.props;
+    // const { title, subtitle, storyline, rating, imagePath } = movie;
     return (
       <div className="movie-card">
         <div className="movie-card-body">
-          <h4>{ title }</h4>
-          <h5>{ subtitle }</h5>
-          <p>{ storyline }</p>
-          <p>{ rating }</p>
+          <h4>{ movie.title }</h4>
+          <h5>{ movie.subtitle }</h5>
+          <p>{ movie.storyline }</p>
+          {/* <p>{ movie.rating }</p> */}
         </div>
-        <img className="movie-card-image" src={ imagePath } alt="imagem" />
+        <img className="movie-card-image" src={ movie.imagePath } alt="imagem" />
       </div>
     );
   }
 }
 
 MovieCard.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  imagePath: PropTypes.string.isRequired,
+  movie: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
 };
+
+// MovieCard.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   subtitle: PropTypes.string.isRequired,
+//   storyline: PropTypes.string.isRequired,
+//   rating: PropTypes.number.isRequired,
+//   imagePath: PropTypes.string.isRequired,
+// };
 
 export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
-import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
       <section className="movie-list">
-        { movies.map(({ title, subtitle, storyline, rating, imagePath }, index) => (
+        { movies.map(({ title, subtitle, storyline, rating, imagePath }) => (
           <MovieCard
-            key={ index }
+            movie={ movies }
+            key={ title }
             title={ title }
             subtitle={ subtitle }
             storyline={ storyline }
@@ -20,5 +22,19 @@ class MovieList extends React.Component {
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        storyline: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        imagePath: PropTypes.string.isRequired,
+      },
+    ),
+  ).isRequired,
+};
 
 export default MovieList;
