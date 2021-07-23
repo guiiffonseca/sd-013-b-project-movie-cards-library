bdbfd179db7aac3f7da627153bf4533f972fd041
Dororops <leeurw@hotmail.com>
2021-07-23 03:30:10 -0300

    Ajustando Imports e Exports

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,15 @@
 // implement MovieList component herea
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return(
+    <header>
+      <h1>Movie Cards Library</h1>
+    </header>
+    )
+        
+  }
+}
+
+export default MovieList;
