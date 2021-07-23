50f89f803ecc8c1982e5cd52b5dce3a11228dda3
Camila Dornas <camiladornas@gmail.com>
2021-07-22 23:19:00 -0300

    Finaliza o projeto

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,30 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// // import movies from '../data';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
 
-// const { movies } = this.props;
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <div>
+        <h4>{ title }</h4>
+        <h5>{ subtitle }</h5>
+        <p>{ storyline }</p>
+        <Rating rating={ rating } />
+        <img src={ imagePath } alt={ title } />
+      </div>
+    );
+  }
+}
 
-// const MovieCard = () => (
-//   <li key={ value }>
-//     {' '}
-//     {value}
-//     {' '}
-//   </li>
-// );
-// export default MovieCard;
+MovieCard.propTypes = {
+  movie: PropTypes.shape({ // Checagem utiliada para objetos
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MovieCard from './MovieCard';
 
 class MovieList extends React.Component {
   render() {
+    const { movies } = this.props;
     return (
-      <body>
-        <MovieCard />
-      </body>
+      <main className="list">
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+      </main>
     );
   }
 }
+
+MovieList.propTypes = {
+  movies: PropTypes.arrayOf( // checagem utilizada para arrays
+    PropTypes.shape({ // Checagem utiliada para objetos
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      storyline: PropTypes.string,
+      rating: PropTypes.number,
+      imagePath: PropTypes.string,
+    }),
+  ).isRequired, // Dessa forma o 'is.Required' pega todo o conjunto de propTypes e aplica a obrigatoriedade.
+};
 
 export default MovieList;
