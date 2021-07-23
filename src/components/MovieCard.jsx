df6eb5362630d66b4829fa0ccfb441454eaff6cf
João Bruno Vasconcelos <joaobruno91@gmail.com>
2021-07-23 10:37:42 -0300

    Adiciona algumas modificaçoes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,30 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+    return (
+      <div className="movie-card">
+        <div className="movie-card-body">
+          <h4>{ title }</h4>
+          <h5>{ subtitle }</h5>
+          <p>{ storyline }</p>
+          <p>{ rating }</p>
+        </div>
+        <img className="movie-card-image" src={ imagePath } alt="imagem" />
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+  imagePath: PropTypes.string.isRequired,
+};
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,28 +1,24 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-// import movies from '../data';
+import MovieCard from './MovieCard';
+import movies from '../data';
 
 class MovieList extends React.Component {
   render() {
-    const { title, subtitle, storyline, rating, imagePath } = this.props;
     return (
-      <div>
-        <p>{ title }</p>
-        <p>{ subtitle }</p>
-        <p>{ storyline }</p>
-        <p>{ rating }</p>
-        <img src={ imagePath } alt="imagem" />
-      </div>
+      <section className="movie-list">
+        { movies.map(({ title, subtitle, storyline, rating, imagePath }, index) => (
+          <MovieCard
+            key={ index }
+            title={ title }
+            subtitle={ subtitle }
+            storyline={ storyline }
+            rating={ rating }
+            imagePath={ imagePath }
+          />
+        )) }
+      </section>
     );
   }
 }
-
-MovieList.propTypes = {
-  title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  storyline: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  imagePath: PropTypes.string.isRequired,
-};
 
 export default MovieList;
