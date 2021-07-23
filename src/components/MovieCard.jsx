626e9e1d1cb5c31827bdbffa5a33399d4010abf3
Mayu <mayusatori@gmail.com>
2021-07-23 10:21:21 -0300

    Finaliza o projeto com o css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,13 +9,13 @@ class MovieCard extends React.Component {
     const { movie } = this.props;
     return (
       <div className="movie-card">
+        <img src={ movie.imagePath } alt="img" className="movie-card-image" />
         <div className="movie-card-body">
-          <img src={ movie.imagePath } alt="img" className="movie-card-image" />
           <h4 className="movie-card-title">{ movie.title }</h4>
           <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
           <p className="movie-card-storyline">{ movie.storyline }</p>
-          <Rating rating={ movie.rating } />
         </div>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
