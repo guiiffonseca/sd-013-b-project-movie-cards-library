d629807fb490b2b8c16883e2e10744c43f20ea9d
Demontier <demontier@outlook.com>
2021-07-24 07:51:02 -0300

    Acrescenta css nos cards

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -6,12 +6,16 @@ class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
     return (
-      <div>
-        <img src={ movie.imagePath } alt="" />
-        <h4>{ movie.title }</h4>
-        <h5>{ movie.subtitle }</h5>
-        <p>{ movie.storyline }</p>
-        <Rating key={ movie.title } rating={ movie.rating } />
+      <div className="movie-card">
+        <img className="movie-card-image" src={ movie.imagePath } alt="" />
+        <div className="movie-card-body">
+          <h4 className="movie-card-title">{ movie.title }</h4>
+          <h5 className="movie-card-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-card-storyline">{ movie.storyline }</p>
+        </div>
+        <div className="movie-card-rating">
+          <Rating key={ movie.title } rating={ movie.rating } />
+        </div>
       </div>
     );
   }
