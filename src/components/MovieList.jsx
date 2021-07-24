28a2b9b635889c0f1be6ea481af5f55e03f9c0e6
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-07-24 20:21:42 -0300

    Teste

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,31 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <section>
+        {movies.map(({ title, subtitle, storyline, rating, imagePath }) => (<MovieCard
+          key={ title }
+          title={ title }
+          subtitle={ subtitle }
+          storyline={ storyline }
+          imagePath={ imagePath }
+          rating={ rating }
+        />))}
+      </section>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+  storyline: PropTypes.string.isRequired,
+  imagePath: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+
+export default MovieList;
