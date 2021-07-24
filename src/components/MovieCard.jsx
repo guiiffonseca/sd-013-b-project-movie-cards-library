40f0ed2adf6b5bb925a1198ae6092472850f924a
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-24 15:28:21 -0300

    Adiciona nota do filme dentro de Render

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,14 +5,14 @@ import Rating from './Rating';
 
 class MovieCard extends Component {
   render() {
-    const { movie: { title, subtitle, storyline, imagePath } } = this.props;
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
     return (
       <div>
         <img src={ imagePath } alt="Movie illustration.png" />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
         <p>{storyline}</p>
-        <Rating />
+        <Rating rating={ rating } />
       </div>
     );
   }
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,12 +1,20 @@
 // implement Rating component here
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends Component {
   render() {
-    return (
-      <span>Rating</span>
-    );
+    const { rating } = this.props;
+    return <span className="rating">{rating}</span>;
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: 0,
+};
 
 export default Rating;
