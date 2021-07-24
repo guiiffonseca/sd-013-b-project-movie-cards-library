7e30c779cb3f0476c9c8b87a850f4c937ae93850
Guilherme Viana <guilhermehviana01@gmail.com>
2021-07-24 14:05:50 -0300

    Cria o componente Rating

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,12 @@
 // implement Rating component here
+import React, { Component } from 'react';
+
+class Rating extends Component {
+  render() {
+    return (
+      <span>Rating</span>
+    );
+  }
+}
+
+export default Rating;
