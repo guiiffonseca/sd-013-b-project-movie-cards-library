68a724692f8e5892c9eeadcff4d0c194e76937bd
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-25 00:23:03 -0300

    Requisitos 3 e 5

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,13 @@
-// Insira aqui o codigo;
+import React from 'react';
+
+class MovieList extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>MovieList</p>
+      </div>
+    );
+  }
+}
+
+export default MovieList;
