f7ca06ded041193ec2999c6ba45652c5e824cc52
gsdu-arte <gabi15duarte@hotmail.com>
2021-07-25 20:31:24 -0300

    Resoluçao requisito 7

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-
+    const { movie: { title, subtitle, storyline, rating, imagePath } } = this.props;
+    return (
+      <div>
+        <img src={ imagePath } alt={ title } className="movie-card-image" />
+      </div>
+    );
   }
 }
 
