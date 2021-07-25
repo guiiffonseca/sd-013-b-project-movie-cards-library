76c27fa3123afd042362d226a5058124354117ff
Guilherme <guilhermerg1234@gmail.com>
2021-07-25 18:32:41 -0300

    Ex9

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,11 +3,13 @@ import React from "react";
 
 const MovieCard = () => {
     let imagePath = 'images/movie_1';
-    let title ='Movie Title 1';
+    let title = 'Movie Title 1';
+    let subtitle = 'Movie Subtitle 1';
 
     return <div>
         <img src={imagePath}></img>
         <h4>{title}</h4>
+        <h5>{subtitle}</h5>
     </div>
 }
 
