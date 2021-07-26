e523681e3dc067dd6b77b4374022d0c670417777
EddieRAM <edusacarneiro@gmail.com>
2021-07-26 19:32:52 -0300

    Requisites 11 and 13 + some css

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -7,10 +8,11 @@ class MovieCard extends React.Component {
     const { title, subtitle, imagePath, storyline } = movie;
     return (
       <section className="movie-card">
-        <img src={ imagePath } alt={ title } />
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
-        <p>{storyline}</p>
+        <img className="movie-card-image" src={ imagePath } alt={ title } />
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <Rating />
       </section>
     );
   }
@@ -22,6 +24,7 @@ MovieCard.propTypes = {
     subtitle: PropTypes.string,
     storyline: PropTypes.string,
     imagePath: PropTypes.string,
+    rating: PropTypes.number,
   }).isRequired,
 };
 
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,13 @@
-// implement Rating component here
+import React from 'react';
+
+class Rating extends React.Component {
+  render() {
+    return (
+      <div className="rating">
+        <h4>Rating</h4>
+      </div>
+    );
+  }
+}
+
+export default Rating;
