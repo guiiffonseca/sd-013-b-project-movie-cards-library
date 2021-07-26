6fe0879a3a729b789e645f37d01727b8840ad603
paulrpn <paulrpn@live.com>
2021-07-26 03:56:42 -0300

    up requisito 1 + up requisito 2  lint

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,39 @@
-// implement MovieList component here
+import React from 'react';
+import PropTypes from 'prop-types';
+// import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies, param } = this.props;
+
+    return (
+      <div>
+        <p>
+          Texto:
+          { param }
+        </p>
+        <p>
+          { typeof movies }
+        </p>
+        {/* <MovieCard /> */}
+      </div>
+    );
+  }
+}
+
+MovieList.propTypes = {
+  param: PropTypes.string.isRequired,
+  movies: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        title: PropTypes.string,
+        subtitle: PropTypes.string,
+        storyline: PropTypes.string,
+        rating: PropTypes.number,
+        imagePath: PropTypes.string,
+      },
+    ),
+  ).isRequired,
+};
+
+export default MovieList;
