48d6a59816825cf86e88be3ea27cc0255bf35d9d
Victor Mendonca <victor.men.dev@gmail.com>
2021-07-26 15:40:01 -0300

    Reqs

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,28 @@
 // implement MovieCard component heere
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends React.Component {
+  render() {
+    const {
+      movie: { title, imagePath },
+    } = this.props;
+    return (
+      <li>
+        <h4>
+          {title}
+        </h4>
+        <img src={ imagePath } alt={ title } />
+      </li>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
