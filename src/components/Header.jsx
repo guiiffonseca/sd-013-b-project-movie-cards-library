6b89001cb4095269cceedc44b1d91ad69509f93a
Heitor Santos <heitorg33@gmail.com>
2021-07-26 19:26:29 -0300

    Requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,14 @@
-// implement Header component here
-//
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    const {headerInfo} = this.props;
+    return (
+      <header>
+        <h1></h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
