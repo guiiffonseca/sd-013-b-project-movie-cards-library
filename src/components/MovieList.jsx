d4129ffef6659f567e4e36263e957cae31406176
Heitor Santos <heitorg33@gmail.com>
2021-07-26 20:46:32 -0300

    Requisito 2

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,20 @@
-// implement MovieList component here
+import React from 'react';
+
+class Movie extends React.Component {
+  render() {
+    const {movieInfo} = this. props
+
+    return (
+      <main>
+        <div>{movieInfo.title}</div>
+        <div>{movieInfo.subtitle}</div>
+        <div>{movieInfo.storyline}</div>
+        <div>{movieInfo.rating}</div>
+        <div>{movieInfo.imagePath}</div>
+      </main>
+    );
+  }
+
+}
+
+export default Movie;
