90c2e1478b239da21ab60b44e9af2796393dbdd3
Guilherme <guilhermerg1234@gmail.com>
2021-07-25 23:51:06 -0300

    Test

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from './Rating';
-
+//
 const MovieCard = (props) => {
   const { movie } = props;
   const { imagePath } = movie;
