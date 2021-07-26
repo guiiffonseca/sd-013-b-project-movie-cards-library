e9c03fae6c2999b03bf89033a96505779454aeef
EddieRAM <edusacarneiro@gmail.com>
2021-07-26 19:10:22 -0300

    Requisite 10 done

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,13 +4,13 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle, imagePath, storyLine } = movie;
+    const { title, subtitle, imagePath, storyline } = movie;
     return (
       <section className="movie-card">
         <img src={ imagePath } alt={ title } />
         <h4>{title}</h4>
         <h5>{subtitle}</h5>
-        <p>{storyLine}</p>
+        <p>{storyline}</p>
       </section>
     );
   }
@@ -20,7 +20,7 @@ MovieCard.propTypes = {
   movie: PropTypes.exact({
     title: PropTypes.string,
     subtitle: PropTypes.string,
-    storyLine: PropTypes.string,
+    storyline: PropTypes.string,
     imagePath: PropTypes.string,
   }).isRequired,
 };
