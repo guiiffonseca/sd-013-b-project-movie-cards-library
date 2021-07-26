5f62f61a088594c0d3dec23450a503fda04552c1
Victor Mendonca <victor.men.dev@gmail.com>
2021-07-26 15:58:24 -0300

    Requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,18 +1,26 @@
 // implement MovieCard component heere
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
     const {
-      movie: { title, imagePath },
+      movie: { title, imagePath, subtitle, storyline, rating },
     } = this.props;
     return (
       <li>
         <h4>
           {title}
         </h4>
+        <h5>
+          {subtitle}
+        </h5>
         <img src={ imagePath } alt={ title } />
+        <p>
+          {storyline}
+        </p>
+        <Rating rating={ rating } />
       </li>
     );
   }
@@ -22,6 +30,9 @@ MovieCard.propTypes = {
   movie: PropTypes.shape({
     title: PropTypes.string,
     imagePath: PropTypes.string,
+    subtitle: PropTypes.string,
+    rating: PropTypes.string,
+    storyline: PropTypes.string,
   }).isRequired,
 };
 
