57b785b749432b2aecd7345b63c9708ddad312d6
Luiz Carlos <luizcarlosvl@hotmail.com>
2021-07-26 21:21:44 -0300

    item 6

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,10 +2,27 @@
 
 import React from 'react';
 
+
+
 class MovieCard extends React.Component {
   render() {
-    return 'teste';
+    const {title, subtitle, storyline, rating, imagePath} = this.props.movie
+    
+    return (
+      <div>
+        <img src={imagePath} alt="capa" />
+        <h4>{title}</h4>
+        <h5>{subtitle}</h5>
+        <p>{storyline}</p>
+      </div>
+    )
   }
 }
 
 export default MovieCard;
+
+
+
+
+
+
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,11 +2,16 @@
 
 import React from 'react';
 import movies from '../data';
+import MovieCard from './MovieCard';
 /* import PropTypes from 'prop-types'; */
 
 class MovieList extends React.Component {
   render() {
-    return movies;
+    return (
+      <div>
+      {movies.map((value) => <MovieCard movie={value}/>)}
+      </div>
+    )  
   }
 }
 
