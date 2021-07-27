f156e6bfe3a0580f2d230d1f2f2d415f2dd06550
Thales Vinicius <thalessilva.tv@gmail.com>
2021-07-27 17:39:47 -0300

    comitao bomba 1 ao 5

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
-// implement MovieCard component here
+import React from 'react';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props;
+
+    return (
+      <div>
+        <img src={ imagePath } alt="" />
+        <h2>{ title }</h2>
+        <p>{ subtitle }</p>
+        <p>{ storyline }</p>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,25 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+
+    return (
+      <section>
+        { movies.map((movie) => (
+          <MovieCard
+            key={ movie.title }
+            imagePath={ movie.imagePath }
+            title={ movie.title }
+            subtitle={ movie.subtitle }
+            storyline={ movie.storyline }
+            rating={ movie.rating }
+          />
+        )) }
+      </section>
+    );
+  }
+}
+
+export default MovieList;
