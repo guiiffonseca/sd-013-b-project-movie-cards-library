7e3c765db7eb5a290e98c4fc561292ffc1c68544
GusMazzinghyDev <gusmazzinghy@gmail.com>
2021-07-27 20:11:11 -0300

    segundo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import movies from './data';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <MovieList movies={ movies } />
-      Hora de codar! 🚀✍️👨‍💻👩‍💻💪
+      <MovieList />
     </div>
   );
 }
