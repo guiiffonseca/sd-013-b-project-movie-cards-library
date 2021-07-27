9353e99e82081b85239a25a68c54e47e84b3d2be
k4nl <gustavoh.bh@gmail.com>
2021-07-27 15:19:34 -0300

    requisito 4,5,6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-// import MovieCard from './components/MovieCard';
-// import MovieList from './components/MovieList';
+import MovieList from './components/MovieList';
+import movies from './data';
 // import Rating from './components/Rating';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <MovieList movies={ movies } />
+      </div>
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,26 @@
-// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import '../App'; // css
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = this.props.movie;
+    return (
+      <article>
+        <span>{ storyline }</span>
+      </article>
+    );
+  }
+}
+
+export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    imagePath: PropTypes.string.isRequired,
+  }).isRequired,
+};
