53843ce1dde7a56321a43b6a4d99447fd7065e50
Natalia Camarinha <natcamarinha@gmail.com>
2021-07-26 21:41:30 -0300

    requisitos 4 e 5

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import MovieList from './components/MovieList';
 function App() {
   return (
     <div className="App">
-      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}
-      Hora de codar! 🚀✍️👨‍💻👩‍💻
+      <Header />
+      <MovieList />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,32 @@
 // implement MovieCard component here
 
 import React from 'react';
+import PropTypes from 'prop-types';
 
+class MovieCard extends React.Component {
+  render() {
+    const { movie } = this.props;
+    return (
+      <div>
+        <img src={ movie.imagePath } alt="img" />
+        <div>
+          <h5>{ movie.title }</h5>  
+          <h6>{ movie.subtitle }</h6>
+          <p>{ movie.storyline }</p>
+        </div>
+      </div>
+    );
+  }
+}
 
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
+
+export default MovieCard;
