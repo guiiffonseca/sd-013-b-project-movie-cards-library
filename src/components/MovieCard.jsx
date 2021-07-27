b87aa6d4a3b1c26fd30c3eaad8b8c11ba01c917b
thiagoteixas <thiagoteixas@gmail.com>
2021-07-27 18:17:49 -0300

    Commitao bomba

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,22 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    const { title, subtitle, storyline, rating, imagePath } = movies;
+    return (
+      <div className="movie-card">
+        <img className="movie-card-image" src={ imagePath } alt="Apenas uma imagem" />
+        <div className="movie-card-information">
+          <h4>{title}</h4>
+          <h5>{subtitle}</h5>
+          <p>{storyline}</p>
+          <span>{rating}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default MovieCard;
