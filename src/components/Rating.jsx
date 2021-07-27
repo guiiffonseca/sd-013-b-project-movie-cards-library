8073b391538d81429a285df53039f6441a1be51c
Heitor Santos <heitorg33@gmail.com>
2021-07-27 19:16:57 -0300

    lint

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,11 @@
-// implement Rating component here
+import React from 'react';
+
+export default class Rating extends React.Component {
+  render() {
+    return (
+      <div>
+        movie
+      </div>
+    );
+  }
+}
