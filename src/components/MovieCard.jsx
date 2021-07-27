0c6af9923072df362ceb3107f4c356ffada5f3b2
Itaopara Neto <itaopara@gmail.com>
2021-07-27 17:33:22 -0300

    Primeiras renderizaçoes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,19 @@
 // implement MovieCard component here
+import React from 'react';
+import movies from '../data';
+
+class MovieCard extends React.Component {
+  render() {
+    return (
+      movies.map(({title, subtitle, imagePath, rating, storyline},index) => (
+        <section key={ index } className="movie-card">
+          <h1>{title}</h1>
+          <h2>{subtitle}</h2>
+          <h2>{storyline}</h2>
+          <h2>{rating}</h2>
+        </section>
+      )));
+  }
+}
+
+export default MovieCard;
