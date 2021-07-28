cd7f6ef8ab84dde8bf85108cd989dd1bd6217f78
Isaddora Bayma <isadora.bayma@gmail.com>
2021-07-28 15:26:38 -0300

    construçao do caminho do  movielist e movie card

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,19 @@
-// implement MovieList component here
+import React from 'react';
+import movies from '../data';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    // const { movies } = this.props;
+    return (
+      <div>
+        {/** verificar a nescessidade dessa div depois */}
+        {movies.map(({ title }) => (
+          <MovieCard key={ title } />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
