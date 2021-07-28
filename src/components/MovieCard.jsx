1d794c1c1cc72c1587cd12383bc58308e8581e1c
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-07-28 18:10:47 -0300

    ajustes em proptypes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
-    const{ title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
     return (
       <div className="movie-card">
         <img src={ imagePath } alt="thumbnail" className="movie-card-image" />
