8276b3fe3947418be483f49eef496d0e60879f37
Daniel Teixeira <daniel.o.teixeira@gmail.com>
2021-07-28 15:53:58 -0300

    Requisitos 2 ao 5

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MovieList from './components/MovieList';
+import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      {
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
         <Header />
-      }
-    </div>
-  );
-}
-
+        <MovieList movies={movies} />
+      </div>
+    );
+  }
+};
 export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,1 +1,26 @@
-// implement MovieList component here
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+
+    return (
+      <div className='movie-list'>
+        {this.props.movies.
+          map(({ title, subtitle, storyline, imagePath, rating }, id) => {
+            return (
+              <MovieCard
+                title={title}
+                subtitle={subtitle}
+                storyline={storyline}
+                imagePath={imagePath}
+                rating={rating}
+                key={id} />
+            )
+          })}
+      </div>
+    )
+  }
+}
+
+export default MovieList;
