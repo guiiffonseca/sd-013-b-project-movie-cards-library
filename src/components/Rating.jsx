1d2d3fd7e921cec07d8d635e08eb4bb90f6a2a45
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-07-28 01:48:32 -0300

    Requisito 12 e arruma css

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Rating extends React.Component {
   render() {
+    const { rating } = this.props;
     return (
-      <div className="rating">
-        Rating
+      <div className="movie-card-rating">
+        <div className="rating">{ rating }</div>
       </div>
     );
   }
 }
+
+Rating.propTypes = {
+  rating: PropTypes.number.isRequired,
+};
 
 export default Rating;
