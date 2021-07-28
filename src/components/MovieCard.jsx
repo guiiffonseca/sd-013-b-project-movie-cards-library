fe06d499d428393801dad55104a677aa00af6e0f
Bernardo Raffide <raffiweed420@gmail.com>
2021-07-28 11:57:17 -0300

    commitao bomba vem

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,1 +1,34 @@
 // implement MovieCard component here
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class MovieCard extends Component {
+  render() {
+    const {
+      movie: { title, subtitle, storyline, imagePath },
+    } = this.props;
+    return (
+      <div className="movieCard">
+        <img src={ imagePath } alt="imagem do filminho" />
+        <div className="movieCardBody">
+          <h4 className="movieCardTitle">{title}</h4>
+          <h5 className="movieCardSubtitle">{subtitle}</h5>
+          <p className="movieCardStoryline">{storyline}</p>
+        </div>
+        {/* <div className="movieCardRating"></div> */}
+      </div>
+    );
+  }
+}
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
+
+export default MovieCard;
