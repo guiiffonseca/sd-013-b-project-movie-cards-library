488a12ed40f8e06753e13e9887d7da3611afd9d3
Pedro Brito <pedroab0119@gmail.com>
2021-07-28 21:37:47 -0300

    Req10 Done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
+import Rating from './components/Rating';
 
 function App() {
   const movies = ['Hasta la vista', 'Volta aqui comigo'];
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Header />
       <MovieList movies={ movies } />
+      <Rating />
     </div>
   );
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 class MovieCard extends React.Component {
   render() {
-    return true;
+    const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+    return (
+      <div>
+        <h4>
+          {title}
+        </h4>
+        <h5>
+          {subtitle}
+        </h5>
+        <p>
+          {storyline}
+        </p>
+        <img src={imagePath}> </img>
+        {rating}
+      </div>
+    );
   }
 }
 
