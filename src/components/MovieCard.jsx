766594bf3b1c0b9cf8ba8502473b27f1a931bcaf
mfelipperd <felipperabelodurans@gmail.com>
2021-07-29 18:55:06 -0300

    rating pronto e o resto dos requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 class MovieCard extends React.Component {
   render() {
@@ -10,7 +11,9 @@ class MovieCard extends React.Component {
         <div className="movie-body">
           <h4 className="movie-title">{ movie.title }</h4>
           <h5 className="movie-subtitle">{ movie.subtitle }</h5>
+          <p className="movie-storyline">{ movie.storyline }</p>
         </div>
+        <Rating rating={ movie.rating } />
       </div>
     );
   }
