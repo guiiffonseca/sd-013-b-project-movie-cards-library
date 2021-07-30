7eb220a6da9c35d353a7ff7f178a682833c2dfd8
Matheus Pormanhani <matheus.pormanhani@gmail.com>
2021-07-30 10:30:56 -0300

    Adiciona atributo 'alt' em <img>

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -8,7 +8,7 @@ class MovieCard extends React.Component {
     const { title, subtitle, storyline, imagePath, rating } = movie;
     return (
       <div className="movie-card movie-card-body">
-        <img src={ imagePath } alt="" className="movie-card-image" />
+        <img src={ imagePath } alt="movie-cover" className="movie-card-image" />
         <h4 className="movie-card-title">{title}</h4>
         <h5 className="movie-card-subtitle">{ subtitle }</h5>
         <p className="movie-card-storyline">{ storyline }</p>
