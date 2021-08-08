ddb3172db3f34d5e809f947c92b5f9d6f672d120
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-08-08 15:11:23 -0300

    fix 1

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class MovieCard extends Component {
+    static propTypes = {
+        prop: movies
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
