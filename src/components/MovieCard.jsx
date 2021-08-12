07578ff44072cc773a1cd4f23e03b4a8431a47a8
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-08-12 15:33:08 -0300

    test11

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,35 @@
+// implement MovieCard component here
+import React from 'react';
+import PropTypes from 'prop-types';
+import Rating from './Rating';
+
+class MovieCard extends React.Component {
+  render() {
+    const { movie: { title, subtitle, storyline, imagePath, rating } } = this.props;
+    return (
+      <section className="movie-card-body">
+        <div className="movie-card">
+          <img src={ imagePath } alt="Poster Movie" className="movie-card-image" />
+          <h4 className="movie-card-title">{ title }</h4>
+          <h5 className="movie-card-subtitle">{ subtitle }</h5>
+          <p className="movie-card-storyline">{ storyline }</p>
+          <div className="movie-card-rating">
+            <Rating rating={ rating } />
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MovieCard;
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+  }).isRequired,
+};
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,1 +1,19 @@
 // implement Rating component here
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Rating(props) {
+  const { rating } = props;
+
+  return (
+    <div className="rating">{ rating }</div>
+  );
+}
+
+Rating.propTypes = {
+  rating: PropTypes.number,
+};
+
+Rating.defaultProps = {
+  rating: null,
+};
